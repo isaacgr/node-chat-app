5a763ac056ccf5d748782c6a28ec49fe43e255c1
isaacgr <isaacgrowell@gmail.com>
2018-10-04 07:23:08 -0400

    Greet a new user and alert others that they joined

--- server/server.js
+++ server/server.js
@@ -14,17 +14,34 @@ app.use(express.static(publicPath));
 io.on("connection", socket => {
   console.log("New user connected");
 
+  socket.emit("newMessage", {
+    from: "admin",
+    text: "Welcome to the chat app",
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit("newMessage", {
+    from: "admin",
+    text: "New user joined",
+    createdAt: new Date().getTime()
+  });
+
   socket.on("disconnect", () => {
     console.log("Client disconnected from server");
   });
 
   socket.on("createMessage", data => {
     console.log("Created message", data);
-    io.emit("newMessage", {
-      from: data.from,
-      text: data.text,
-      createdAt: new Date().getTime()
-    });
+    // io.emit("newMessage", {
+    //   from: data.from,
+    //   text: data.text,
+    //   createdAt: new Date().getTime()
+    // });
+    // socket.broadcast.emit("newMessage", {
+    //   from: data.from,
+    //   text: data.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 });
 
